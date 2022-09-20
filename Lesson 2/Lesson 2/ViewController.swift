976faccc0d8f36f5628fdf65677cfb6f12a66c1f
Christopher Tan <72955765+Christopher-Tan@users.noreply.github.com>
2022-09-19 20:02:32 -0400

    First two lessons

---
+++ Lesson 2/Lesson 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2
+//
+//  Created by Christopher Tan on 9/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
